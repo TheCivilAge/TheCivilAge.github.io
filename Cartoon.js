ff38bbf379e13a625e9a41de301b57d3f4a2fcfa
Katya Civil <TheCivilAge@gmail.com>
2022-02-28 16:49:08 -0500

    Lab 1 1st draft

--- Cartoon.js
+++ Cartoon.js
@@ -9,15 +9,16 @@ var fruit = [
 	{name:"Apple", quantity:20, color:"red"},
 	{name:"Orange", quantity:10, color:"orange"},
 	{name:"banana", quantity:15, color:"yellow"},
-	{name:"kiwi", quantity:3, color:"yellow"},
+	{name:"kiwi", quantity:3, color:"green"},
 	{name:"blueberry", quantity:5, color:"blue"},
 	{name:"grapes", quantity:8, color:"purple"}
 ];
 ctx.textAlign="center";
 for(let i=0;i<fruit.length;i++){
-    //ctx.fillStyle("black");
-    ctx.fillText(fruit[i].name,(100+i*80),510);
-    //ctx.fillStyle(fruit[i].color);
+    ctx.fillStyle=fruit[i].color;
     ctx.fillRect((50+i*80),500,80,(fruit[i].quantity*-20));
 
+    ctx.fillStyle="black";
+    ctx.fillText(fruit[i].name,(90+i*80),510);
+    ctx.fillText(fruit[i].quantity,(90+i*80),480);
 }
