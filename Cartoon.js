8d82071290a19348cf1930b110b472186d58d83a
Katya Civil <TheCivilAge@gmail.com>
2022-02-28 16:36:46 -0500

    Added cartoon.js
    graph presented correctly except fillstyles not working on rect and text.
    Add indents to x and y axis to show values or print text in rect.

---
+++ Cartoon.js
@@ -0,0 +1,23 @@
+var canvas =document.getElementById("canvas");
+var ctx =canvas.getContext("2d");
+ctx.moveTo(50, 50);
+ctx.lineTo(50, 500);
+ctx.lineTo(600,500);
+ctx.stroke();
+
+var fruit = [
+	{name:"Apple", quantity:20, color:"red"},
+	{name:"Orange", quantity:10, color:"orange"},
+	{name:"banana", quantity:15, color:"yellow"},
+	{name:"kiwi", quantity:3, color:"yellow"},
+	{name:"blueberry", quantity:5, color:"blue"},
+	{name:"grapes", quantity:8, color:"purple"}
+];
+ctx.textAlign="center";
+for(let i=0;i<fruit.length;i++){
+    //ctx.fillStyle("black");
+    ctx.fillText(fruit[i].name,(100+i*80),510);
+    //ctx.fillStyle(fruit[i].color);
+    ctx.fillRect((50+i*80),500,80,(fruit[i].quantity*-20));
+
+}
