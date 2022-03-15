77c353862e8f643ac6715e658ecb6eeb2838744a
Katya Civil <TheCivilAge@gmail.com>
2022-03-14 21:34:47 -0400

    updated Animation.js

---
+++ Homeworks/Animation.js
@@ -0,0 +1,9 @@
+var c =document.getElementById("Animation");
+var ctx =c.getContext("2d");
+
+var img= new Image();
+img.src='sprite\222-2223669_sonic-sprite-sheet-png-transparent-png.png';
+img.onload= function(){
+    ctx.drawImage(img,0,0,100,100,0,0,100,100);
+}
+
